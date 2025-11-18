b2764eac0d9e24effc8a245288dcf3c8c307c391
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-18 12:28:09 +0530

    Add files via upload

---
+++ B.C
@@ -0,0 +1,12 @@
+/* 
+Q2: Write a C program to take two integers as input and print their sum.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("Sum = %d\n", a + b);
+    return 0;
+}
