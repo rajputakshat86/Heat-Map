34b73ba6c29af842b4afd3e3d453cf68463f9b1d
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-20 21:39:05 +0530

    Add files via upload

---
+++ H.C
@@ -0,0 +1,20 @@
+/*
+Q2: Write a C program to find the largest among three numbers.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a >= b && a >= c)
+        printf("Largest = %d\n", a);
+    else if(b >= a && b >= c)
+        printf("Largest = %d\n", b);
+    else
+        printf("Largest = %d\n", c);
+
+    return 0;
+}
