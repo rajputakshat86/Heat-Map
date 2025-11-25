a75933256649f130880c356ffbb4923f04fa48d0
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-25 16:49:35 +0530

    Add files via upload

---
+++ CC.C
@@ -0,0 +1,29 @@
+/*
+Q3: Write a C program to check whether a given number is prime or not.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1)
+        flag = 0;
+    else {
+        for (i = 2; i <= n/2; i++)
+            if (n % i == 0) {
+                flag = 0;
+                break;
+            }
+    }
+
+    if (flag)
+        printf("%d is a prime number.\n", n);
+    else
+        printf("%d is not a prime number.\n", n);
+
+    return 0;
+}
