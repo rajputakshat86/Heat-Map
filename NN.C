3be863817e1eb36ae1ca5bbf5e55f9f59af1ac27
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-27 09:56:58 +0530

    Add files via upload

---
+++ NN.C
@@ -0,0 +1,19 @@
+/* 
+Q2: Write a C program to input 3 numbers and print the largest number among them.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b && a > c)
+        printf("Largest: %d", a);
+    else if(b > c)
+        printf("Largest: %d", b);
+    else
+        printf("Largest: %d", c);
+
+    return 0;
+}
