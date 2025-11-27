89761bc117663f066d201eef806ba4b2e2cd4978
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-27 09:57:31 +0530

    Add files via upload

---
+++ PP.C
@@ -0,0 +1,20 @@
+/*
+Q4: Write a C program to count the total number of vowels in a string.
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+    scanf("%[^\n]", str);
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+           str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+            count++;
+    }
+
+    printf("Vowels: %d", count);
+    return 0;
+}
