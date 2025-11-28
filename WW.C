6e60dd2cbbcacf726c83eaaff1fe4865c6115fa1
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-28 10:51:12 +0530

    Add files via upload

---
+++ WW.C
@@ -0,0 +1,19 @@
+/*
+Q1: Write a C program to swap two numbers using a function.
+*/
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swap: x = %d, y = %d\n", x, y);
+    return 0;
+}
