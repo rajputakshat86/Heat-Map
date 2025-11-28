b252419e2fc665b593f253087a4ce28d5a25236b
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-28 10:53:02 +0530

    Add files via upload

---
+++ ZZ.C
@@ -0,0 +1,17 @@
+/*
+Q4: Write a C program to calculate the sum of all elements in an array.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    for(int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    printf("Sum of array elements: %d\n", sum);
+    return 0;
+}
