cad7855ad2c4769c77dfddf676b28a9c0c2d163f
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-29 12:17:24 +0530

    Add files via upload

---
+++ JJJ.C
@@ -0,0 +1,22 @@
+/*
+Q155: Write a C program to reverse a string without using library functions.
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int len = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[len] != '\0')
+        len++;
+
+    printf("Reversed string: ");
+    for (int i = len - 1; i >= 0; i--)
+        printf("%c", str[i]);
+
+    return 0;
+}
