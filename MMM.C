1fffe150f33f8c8b7ebc81c5cbf2095cd82c4b32
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-30 13:34:01 +0530

    Add files via upload

---
+++ MMM.C
@@ -0,0 +1,10 @@
+// Question: Write a C program to read two integers and print their sum.
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+    return 0;
+}
