e678ffd35d26cd499ac35efe01100baca3dd9f84
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-30 13:35:12 +0530

    Add files via upload

---
+++ PPP.C
@@ -0,0 +1,13 @@
+// Question: Write a C program to print the first N natural numbers.
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for(int i = 1; i <= N; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
