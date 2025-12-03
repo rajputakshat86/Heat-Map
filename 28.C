e71dd074f5cbba3edf2dafeeb3f85ccc130dea4f
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-12-03 11:33:21 +0530

    Add files via upload

---
+++ 28.C
@@ -0,0 +1,19 @@
+/*
+QUESTION:
+Write a C program to reverse a given integer number.
+*/
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        rev = rev * 10 + (n % 10);
+        n /= 10;
+    }
+
+    printf("Reversed number = %d", rev);
+    return 0;
+}
