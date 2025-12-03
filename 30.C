b6df1ed0ee9a6a75818dbe33cfb16b6c68f82936
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-12-03 11:34:02 +0530

    Add files via upload

---
+++ 30.C
@@ -0,0 +1,24 @@
+/*
+QUESTION:
+Write a C program to print first N terms of the Fibonacci series.
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    int a = 0, b = 1, c;
+
+    printf("%d %d ", a, b);
+
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
